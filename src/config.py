94e5ca66d527291cfa2023493eec10eb2df902a1
Maxim Novic <mxnvkf@gmail.com>
2024-05-02 19:15:27 +0300

    footer + navbar

--- src/config.py
+++ src/config.py
@@ -7,17 +7,19 @@ class Settings(BaseSettings):
     DB_USER: str
     DB_PASS: str
     DB_NAME: str
+    FOOTER_NAME: str
+    TELEGRAM_URL: str
+    GITHUB_URL: str
 
     page_title: str = 'Карточки - конструктор курсов'
     logo_src: str = 'https://starove.ru/wp-content/uploads/2019/05/az-03.jpg'
-    footer_text: str = 'Карточки - конструктор курсов для запоминания и обучения, Максим Н.Л., 2024 год'
+    footer_text: str = f'Карточки - конструктор курсов для запоминания и обучения, '
     help_page_text: str = '??? please write here :)'
 
     @property
     def DATABASE_URL_asyncpg(self):
         # postgresql+asyncpg://postgres:postgres@localhost:5432/sa
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
-
 
     model_config = SettingsConfigDict(env_file='.env')
 
--- src/pages/createpage.py
+++ src/pages/createpage.py
@@ -1,15 +1,19 @@
 from __future__ import annotations as _annotations
+
+import datetime
+
 from fastui.components.display import DisplayMode, DisplayLookup
 from fastui import AnyComponent
 from fastui import components as c
 from fastui.events import GoToEvent
 from src.config import settings
+from datetime import date
 
 navbar_dict = {'Курсы': '/courses/', 'Пользователи':'/users/', 'Помощь': '/help/', 'Войти': '/auth/'}
 """
 Navbar demo меню войти если logout иначе имя
 """
-footer_dict = {'Github': 'https://github.com/MxRight/cards_course_maker', 'Telegram': 'https://t.me/max_right'}
+footer_dict = {'Github': settings.GITHUB_URL, 'Telegram': settings.TELEGRAM_URL}
 
 
 def create_page(*components: AnyComponent, title: str | None = None) -> list[AnyComponent]:
@@ -35,7 +39,7 @@ def create_page(*components: AnyComponent, title: str | None = None) -> list[Any
                 ],
             ),
             c.Footer(
-                extra_text=settings.footer_text,
+                extra_text=settings.footer_text + settings.FOOTER_NAME + ', ' + str(date.today().year),
                 links=[
                     c.Link(
                         components=[c.Text(text=title)],
